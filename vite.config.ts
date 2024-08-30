49959ae564e16428bf9718f4e80b633d97e6ce56
GofurovIsroil <isroilgofurov0099@gmail.com>
2024-08-30 20:51:24 +0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  // Add server configuration to enable better debugging
+  server: {
+    port: 3000, // Set the port you want to use
+    open: true, // Automatically open the browser on server start
+  },
+  build: {
+    target: "esnext", // Ensure compatibility with modern browsers
+  },
+});
